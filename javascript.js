4178da6592982a2f55f52834d91dd680e0cf6b53
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-13 05:39:18 +1300

    made unbeatable bot

--- javascript.js
+++ javascript.js
@@ -2,21 +2,19 @@
 let symb;
 let textContainer = document.querySelector('.textContainer');
 let gameover = false;
-function player(symbol, moves){
+function curPlayer(symbol, moves){
         const playerSymbol = symbol;
         const playerMoves = moves;
         return { playerSymbol, playerMoves};
     }
-    const playerX = player('X', []);
-    const playerO = player('O', []);
-
+    const playerX = curPlayer('X', []);
+    const playerO = curPlayer('O', []);
 const gameBoard = (() => {
     let boardArr = [];
     for(i=1;i<10;i++){
         boardArr.push('');
     }
     symb = 'X';
-    
     let boardElement = document.querySelector('.gameBoard');
     for(let tileNum in boardArr){
         let tile = document.createElement('div');
@@ -31,7 +29,7 @@ const gameBoard = (() => {
             if(symb == 'X'){
                 playerX.playerMoves.push(Number(tile.id));
                 if((playerX.playerMoves).length >= 3){
-                    winCheck(playerX.playerMoves);
+                    checkWin(playerX.playerMoves);
                 }
                 symb = 'O';
                 if(gameover != true){
@@ -59,129 +57,99 @@ const gameBoard = (() => {
         boardElement.appendChild(tile);
     }
 });
-function winCheck(examArray){
-    console.log('checkStart')
-    console.log(examArray);
-    let matchedNums = 0;
-    const winningMoves = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7],];
-    for(i=0; i<8; i++){
-        matchedNums = 0;
-        for(let moveCheck in examArray){
-            if(winningMoves[i].includes(examArray[moveCheck])){
-                matchedNums++;
-            }
-        }
-        if (matchedNums > 2){
-            gameover = true;
-            break;
-        }
+function makeMove(tileId, symbol) {
+    if (symbol == 'X') {
+        playerX.playerMoves.push(tileId);
+    } else {
+        playerO.playerMoves.push(tileId);
     }
-}
-let compArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let ranNum;
-function computer(){
-    symb = 'O';
-    let acceptedNum = false;
-    while(acceptedNum == false){
-        ranNum = Math.floor(Math.random() * 10);
-        if(ranNum != 0 && !playerX.playerMoves.includes(ranNum) && !playerO.playerMoves.includes(ranNum)) acceptedNum = true;
-    }
-    console.log(ranNum);
-    let compTile = document.getElementById(ranNum);
-    compTile.textContent = symb;
-    playerO.playerMoves.push(ranNum);
-    console.log(playerO.playerMoves);
-    if((playerO.playerMoves).length >= 3){
-        winCheck(playerO.playerMoves);
-    }
-    symb = 'X';
-    compTile.style.pointerEvents = 'none';
-    textContainer.textContent = `Player ${symb}'s turn!`;
-    if (gameover == true){
-        textContainer.textContent = `O WINS!!!!`;
-    }
-}
-const oButton = document.getElementById('oBut');
-        oButton.addEventListener('click', () =>{
+    if (checkWin(playerX.playerMoves)) {
+        textContainer.textContent = 'X WINS!!!!';
+        gameover = true;
+    } else if (checkWin(playerO.playerMoves)) {
+        textContainer.textContent = 'O WINS!!!!';
+        gameover = true;
+    } else if (playerX.playerMoves.length + playerO.playerMoves.length === 9) {
+        textContainer.textContent = 'It\'s a draw!';
+        gameover = true;
+    } else {
+        symb = symb === 'X' ? 'O' : 'X';
+        textContainer.textContent = `Player ${symb}'s turn!`;
+        if (symb === 'O') {
             computer();
-            
-        });
+        }
+    }
+}
+function checkWin(moves) {
+    const winningMoves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+    return winningMoves.some((combo) => combo.every((move) => moves.includes(move)));
+}
+function computer() {
+    symb = 'O';
+    const bestMove = findBestMove(playerO.playerMoves, playerX.playerMoves);
+    const compTile = document.getElementById(bestMove);
+    compTile.textContent = symb;
+    makeMove(bestMove, symb);
+}
+function findBestMove(playerMoves, opponentMoves) {
+    const availableSpots = emptySpots();
+    let bestScore = -Infinity;
+    let bestMove;
+    for (let i = 0; i < availableSpots.length; i++) {
+        const move = availableSpots[i];
+        playerMoves.push(move);
+        const score = minimax(playerMoves, opponentMoves, false);
+        playerMoves.pop();
+        if (score > bestScore) {
+            bestScore = score;
+            bestMove = move;
+        }
+    }
+    return bestMove;
+}
+function minimax(playerMoves, opponentMoves, isMaximizing) {
+    const scores = {
+        X: -1,
+        O: 1,
+        draw: 0
+    };
+    if (checkWin(opponentMoves)) {
+        return -scores[symb];
+    } else if (checkWin(playerMoves)) {
+        return scores[symb];
+    } else if (playerMoves.length + opponentMoves.length === 9) {
+        return scores.draw;
+    }
+    const availableSpots = emptySpots();
+    let bestScore = isMaximizing ? -Infinity : Infinity;
+    for (let i = 0; i < availableSpots.length; i++) {
+        const move = availableSpots[i];
+        if (isMaximizing) {
+            playerMoves.push(move);
+        } else {
+            opponentMoves.push(move);
+        }
+        const score = minimax(playerMoves, opponentMoves, !isMaximizing);
+        if (isMaximizing) {
+            bestScore = Math.max(score, bestScore);
+        } else {
+            bestScore = Math.min(score, bestScore);
+        }
+        if (isMaximizing) {
+            playerMoves.pop();
+        } else {
+            opponentMoves.pop();
+        }
+    }
+    return bestScore;
+}
+function emptySpots() {
+    const emptySpotsArr = [];
+    for (let i = 1; i < 10; i++) {
+        if (!playerO.playerMoves.includes(i) && !playerX.playerMoves.includes(i)) {
+            emptySpotsArr.push(i);
+        }
+    }
+    return emptySpotsArr;
+}
 gameBoard();
-let emptySpotArr;
-function emptySpots(){
-    emptySpotArr = [];
-    for(i=1; i<10; i++){
-        if(!playerO.playerMoves.includes(i) && !playerX.playerMoves.includes(i)){
-            emptySpotArr.push(i);
-        }
-    }
-    return(emptySpotArr);
-}
-
-function minimax(newBoard, player) {
-    let availSpots = emptySpots();
-}
-function boardAscii(){
-    let boardAsciiArr = [];
-    let rowNum = 0;
-    for(i=0;i<3;i++){
-        let arrRow = [];
-        for(a=1;a<4;a++){
-            if(playerO.playerMoves.includes(a+rowNum)){
-                arrRow.push('O');
-            }
-            else if(playerX.playerMoves.includes(a+rowNum)){
-                arrRow.push('X');
-            }
-            else{
-                arrRow.push('-');
-            }
-            
-            
-        }
-        boardAsciiArr.push(arrRow);
-        rowNum+= 3;
-    }
-    return(boardAsciiArr);
-}
-
-function evaluate(board){
-    for(i=0;i<3;i++){
-        if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-            if(board[i][0] == "O"){
-            return +10;
-            }
-            else if(board[i][0] == "X"){
-                return -10;
-            }
-        }
-    }
-    for(x=0;x<3;x++){
-        if(board[0][x] == board[1][x] && board[1][x] == board[2][x]){
-            if(board[0][x] == "O"){
-                return +10;
-                }
-                else if(board[0][x] == "X"){
-                    return -10;
-                }
-        }
-    }
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
-        if(board[0][0] == "O"){
-            return +10;
-        }
-        else if(board[0][0] == "X"){
-            return -10;
-        }
-    }
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
-        if(board[0][2] == "O"){
-            return +10;
-        }
-        else if(board[0][2] == "X"){
-            return -10;
-        }
-    }
-    return 0;
-}
-let boardValue = (evaluate(boardAscii()));
