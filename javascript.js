dd138c948186315b42a62520b82340cc966a2c7d
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-14 00:36:08 +1300

    added difficulties

--- javascript.js
+++ javascript.js
@@ -1,29 +1,25 @@
 let symb;
 let textContainer = document.querySelector('.textContainer');
 let gameover = false;
-let difficulty = 'medium';
+let difficulty = 'impossible';
 function curPlayer(symbol, moves) {
     const playerSymbol = symbol;
     const playerMoves = moves;
     return { playerSymbol, playerMoves };
 }
-
 const playerX = curPlayer('X', []);
 const playerO = curPlayer('O', []);
-
 const gameBoard = () => {
     let boardArr = [];
     for (let i = 1; i < 10; i++) {
         boardArr.push('');
     }
     symb = 'X';
-
     let boardElement = document.querySelector('.gameBoard');
     for (let tileNum in boardArr) {
         let tile = document.createElement('div');
         tile.setAttribute('id', `${Number(tileNum) + 1}`);
         tile.setAttribute('class', 'tile');
-
         function tileClick() {
             if (gameover == true) {
                 return '';
@@ -54,7 +50,6 @@ const gameBoard = () => {
             }
             if (gameover != true) textContainer.textContent = `Player ${symb}'s turn!`;
         }
-
         tile.addEventListener('click', function () {
             tileClick();
         }, {
@@ -63,7 +58,6 @@ const gameBoard = () => {
         boardElement.appendChild(tile);
     }
 };
-
 function makeMove(tileId, symbol) {
     if (symbol == 'X') {
         playerX.playerMoves.push(tileId);
@@ -77,7 +71,7 @@ function makeMove(tileId, symbol) {
         textContainer.textContent = 'O WINS!!!!';
         gameover = true;
     } else if (playerX.playerMoves.length + playerO.playerMoves.length === 9) {
-        textContainer.textContent = 'It\'s a draw!';
+        textContainer.textContent = "It's a draw!";
         gameover = true;
     } else {
         symb = symb === 'X' ? 'O' : 'X';
@@ -87,6 +81,11 @@ function makeMove(tileId, symbol) {
         }
     }
 }
+let ranNum; 
+let rng;
+let ranMove;
+let ranTile;
+let ranNumChosen;
 
 function checkWin(moves) {
     const winningMoves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
@@ -94,13 +93,29 @@ function checkWin(moves) {
 }
 
 function computer() {
+    ranNumChosen = false;
     symb = 'O';
-    let rng = Math.floor(Math.random() * 4);
-    const bestMove = findBestMove(playerO.playerMoves, playerX.playerMoves, 0);
-    const compTile = document.getElementById(bestMove);
-    compTile.textContent = symb;
-    compTile.style.pointerEvents = 'none';
-    makeMove(bestMove, symb);
+    rng = Math.floor(Math.random() * 3);
+    while(ranNumChosen == false){
+        ranNum = (Math.floor(Math.random() * 9) + 1);
+        if(!playerO.playerMoves.includes(ranNum) && !playerX.playerMoves.includes(ranNum)){
+            ranNumChosen = true;
+        }
+    }
+    if(rng == 2 && difficulty == 'medium'){
+        ranMove = ranNum;
+        ranTile = document.getElementById(ranMove);
+        ranTile.textContent = symb;
+        ranTile.style.pointerEvents = 'none';
+        makeMove(ranMove, symb);
+    }
+        else if(difficulty == 'impossible' || difficulty == 'medium'){
+        const bestMove = findBestMove(playerO.playerMoves, playerX.playerMoves, 0);
+        const compTile = document.getElementById(bestMove);
+        compTile.textContent = symb;
+        compTile.style.pointerEvents = 'none';
+        makeMove(bestMove, symb);
+    }
 }
 
 function findBestMove(playerMoves, opponentMoves, depth) {
@@ -126,7 +141,6 @@ function evaluate(playerMoves, opponentMoves) {
         O: 1,
         draw: 0
     };
-
     if (checkWin(opponentMoves)) {
         return -scores[symb];
     } else if (checkWin(playerMoves)) {
@@ -138,7 +152,6 @@ function evaluate(playerMoves, opponentMoves) {
     }
 }
 
-
 function minimax(playerMoves, opponentMoves, depth, isMaximizer) {
     const availableSpots = emptySpots();
     const scores = {
@@ -146,11 +159,9 @@ function minimax(playerMoves, opponentMoves, depth, isMaximizer) {
         O: 1,
         draw: 0
     };
-
     if (depth === 0 || checkWin(playerMoves) || checkWin(opponentMoves) || availableSpots.length === 0) {
         return evaluate(playerMoves, opponentMoves);
     }
-
     if (isMaximizer) {
         let bestScore = -Infinity;
         for (let i = 0; i < availableSpots.length; i++) {
@@ -183,5 +194,21 @@ function emptySpots() {
     }
     return emptySpotsArr;
 }
-
-gameBoard();
+let celCatCont = document.querySelector('.cat');
+let catImg = document.querySelector('.catImg');
+let medButton = document.getElementById('med');
+let impossibleButton = document.getElementById('imp');
+medButton.addEventListener('click', () => {
+    difficulty = 'medium';
+    gameBoard();
+}, {once: true});
+impossibleButton.addEventListener('click', () => {
+    difficulty = 'impossible';
+    document.body.setAttribute('class', 'backgroundChange');
+    celCatCont.style.display = 'flex';
+    setTimeout(() => {
+        catImg.style.visibility = 'visible';
+    }, 1200);
+    textContainer.style.backgroundColor = 'rgb(210, 217, 248)';
+    gameBoard();
+}, {once: true});
